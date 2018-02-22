659063156b47f8e41e02a4c66e37fec89ad4cc25
Mike G <gugmi01@gmail.com>
2018-02-22 15:44:17 -0600

    Bug fixes

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -5,30 +5,26 @@ module.exports = {
     const schema = {
       email: Joi.string().email(),
       password: Joi.string().regex(
-        new RegExp('^[a-zA-Z0-9]{8-32}$')
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
       )
     }
 
-    const {
-      error,
-      value
-    } = Joi.validate(req.body, schema)
+    const {error} = Joi.validate(req.body, schema)
 
     if (error) {
       switch (error.details[0].context.key) {
         case 'email':
           res.status(400).send({
-            error: 'Please provide a valid email address'
+            error: 'You must provide a valid email address'
           })
-          console.log(value)
           break
         case 'password':
           res.status(400).send({
             error: `The password provided failed to match the following rules:
               <br>
-              1. It must contain ONLY the following characters: lower case, upper case, and numerics
+              1. It must contain ONLY the following characters: lower case, upper case, numerics.
               <br>
-              2. It must be at least 8 characters in lengths and not greater than 32 characters
+              2. It must be at least 8 characters in length and not greater than 32 characters in length.
             `
           })
           break
